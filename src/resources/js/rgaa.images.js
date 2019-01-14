599dc0f4496c65c81f1aca70dcbbdf0811d83b7a
Jonathan JACQUES <jjprods@gmail.com>
2019-01-14 17:08:59 +0100

    rgaa.core

--- src/resources/js/rgaa.images.js
+++ src/resources/js/rgaa.images.js
@@ -1,13 +1,6 @@
-// ==UserScript==
-// @name         RGAA Checker - Images
-// @version      0.1
-// @description  Validation automatisée RGAA
-// @author       Jonathan JACQUES
-// @include *
-// @grant none
-// ==/UserScript==
 (function () {
     "use strict";
+
     window.rgaa.image = {
         test: function () {
             alert ("ok");
